bbe9785ec29f7cc4de26ffecf2e5332514fdeb3f
jancis <janciskauls@gmail.com>
2024-10-26 18:32:52 +0300

    completed and succesfully tested 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(givenString) {
 
-};
+    return givenString.split('').reverse().join('');
 
+    //solution can be found in a simple booke "Javascript notes for profesionals"
+}
 // Do not edit below this line
 module.exports = reverseString;
