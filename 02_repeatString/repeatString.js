7edfd43d643b8da7977f66f33bab5ef6d146981f
jancis <janciskauls@gmail.com>
2024-10-26 18:22:57 +0300

    finished and tested succesfully 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(givenString, times) {
+    let sumString = "";
 
+    if (times < 0) {
+        return "ERROR";
+    }
+
+    for (i=0; i < times; i++) {
+        sumString = sumString + givenString;
+    }
+    return sumString;
 };
 
 // Do not edit below this line
