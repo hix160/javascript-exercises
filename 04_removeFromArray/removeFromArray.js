74f7d7e3bbbfc3588aad9edd9e80b6cd93b57fab
jancis <janciskauls@gmail.com>
2024-10-28 11:17:08 +0200

    finished and succefully tested 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    var myArray = [];
+    for(i=0; i<array.length; i++) {
+        if (!args.includes(array[i])) {
+            myArray.push(array[i]);
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//removeFromArray([1,2,3], 3);
