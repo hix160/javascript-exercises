05a19176010612770369a31e4e3e5d26fb54fd34
jancis <janciskauls@gmail.com>
2024-10-28 12:54:12 +0200

    finished and succefully tested 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  
+  var celsiusValue = (value - 32) / (9/5);
+  celsiusValue = parseFloat(celsiusValue.toFixed(1));
+  return celsiusValue;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  var farenheitValue = value * 9/5 +32;
+  farenheitValue = parseFloat(farenheitValue.toFixed(1));
+  return farenheitValue
 };
 
 // Do not edit below this line
