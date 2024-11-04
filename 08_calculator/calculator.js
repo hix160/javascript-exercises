c8b14d98da549d20c32a6f8d3c951c5cc265d328
jancis <janciskauls@gmail.com>
2024-11-04 16:57:38 +0200

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for(i=0; i < array.length; i++){
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for(i=0; i < array.length; i++) {
+    result = result * array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let result = 1;
+	for(i=0; i<power; i++) {
+     result = result *number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  let result = 1;
+	for(;factorial > 0; factorial--) {
+    result = result * factorial
+  }
+  return result;
 };
 
 // Do not edit below this line
